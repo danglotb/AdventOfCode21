35a3f4b518009928ba03f45b95f66339e17bb7ef
danglotb <benjamin.danglot@davidson.fr>
2021-12-02 22:01:18 +0100

    puzzle1: add second part

---
+++ puzzle_1_2.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+
+/**
+ * Main function of the script
+ */
+function main() {
+	fs.readFile(
+		//'data/input/example_puzzle_1',
+		'data/input/puzzle_1',
+		'utf8' , (err, data) => {
+		if (err) {
+		  console.error(err)
+		  return
+		}
+		data = data.split('\n');
+		var nbIncrease = 0;
+		for (let i = 0 ; i < data.length - 2; i++) {
+			const window1 = +data[i] + +data[i + 1] + +data[i + 2];
+			const window2 = +data[i + 1] + +data[i + 2] + +data[i + 3];
+			if (window2 > +window1) {
+				nbIncrease += 1;
+			}
+		}
+		console.log(nbIncrease);
+	})
+}
+
+main();
